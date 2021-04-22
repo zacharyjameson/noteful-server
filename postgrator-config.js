3df2b0ca14a36be9975a46ae9e1e1b810ae9a403
Zack Jameson <zacharymjameson@gmail.com>
2021-04-22 19:09:53 -0400

    pg script update

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,7 +8,7 @@ module.exports = {
   port: "5432",
   user: "dhxmiyaycrwnml",
   password: "9b2c8717fd0a88ba5b22261e3ac884b36580f21c88b984f6d83391689008b86f",
-  sslmode: "true",
+  sslmode: true,
   connectionUrl:
     "postgres://dhxmiyaycrwnml:9b2c8717fd0a88ba5b22261e3ac884b36580f21c88b984f6d83391689008b86f@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d34si77rr9hnid",
 };
