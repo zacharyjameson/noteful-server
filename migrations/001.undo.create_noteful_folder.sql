a2d69677ccaca6aa8d8001dc5a8a2889a5427416
Zack Jameson <zacharymjameson@gmail.com>
2021-04-22 19:14:07 -0400

    postgrator test

--- migrations/001.undo.create_noteful_folder.sql
+++ migrations/001.undo.create_noteful_folder.sql
@@ -1,2 +1,2 @@
-DROP TABLE IF EXISTS noteful_note;
+/*DROP TABLE IF EXISTS noteful_note;
 DROP TABLE IF EXISTS noteful_folder;
--- postgrator-config.js
+++ postgrator-config.js
@@ -3,12 +3,11 @@ require("dotenv").config();
 module.exports = {
   migrationDirectory: "migrations",
   driver: "pg",
-  dbname: "d34si77rr9hnid",
-  host: "ec2-34-225-167-77.compute-1.amazonaws.com",
-  port: "5432",
-  user: "dhxmiyaycrwnml",
-  password: "9b2c8717fd0a88ba5b22261e3ac884b36580f21c88b984f6d83391689008b86f",
-  ssl: true,
-  connectionUrl:
-    "postgres://dhxmiyaycrwnml:9b2c8717fd0a88ba5b22261e3ac884b36580f21c88b984f6d83391689008b86f@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d34si77rr9hnid",
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
